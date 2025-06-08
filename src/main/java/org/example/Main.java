304447d6e992d079570007f46dd77c1b34bed677
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-06-08 12:10:26 -0300

    Prueba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ public class Main {
             // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            System.out.println(10);
         }
     }
 }
