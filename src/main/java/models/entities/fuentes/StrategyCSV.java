e9f5a2e1105ad75d0430c7c87515ffc349fa29c9
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-06-08 21:10:34 -0300

    falta lo de categoria y ubicacion x las etiquetas

--- src/main/java/models/entities/fuentes/StrategyCSV.java
+++ src/main/java/models/entities/fuentes/StrategyCSV.java
@@ -1,19 +1,45 @@
 package models.entities.fuentes;
 
+import com.opencsv.CSVReader;
 import models.entities.hecho.Hecho;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StrategyCSV implements StrategyTipoConexion {
 
+    private static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     @Override
-    public void agregarHecho() {}
-}
+    public Map<String, Hecho> agregarHecho(String FuenteBase) {
+
+        Map<String, Hecho> hechos = new HashMap<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(FuenteBase))) {
+            String[] fila;
+            reader.readNext(); // Saltar encabezado
+
+            while ((fila = reader.readNext()) != null) {
+                if (fila.length != 6) {
+                    System.out.println("Línea ignorada: no tiene 6 columnas.");
+                    continue;
+                }
+
+                String titulo = fila[0].trim();
+                String descripcion = fila[1].trim();
+                LocalDate fecha = LocalDate.parse(fila[5].trim(), FORMATO_FECHA);
+
+                Hecho hecho = new Hecho(titulo, descripcion, fecha);
+                hechos.put(titulo, hecho);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return hechos;
+    }
+
+    }
