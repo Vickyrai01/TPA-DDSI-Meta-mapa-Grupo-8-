61571637326f445b4efb453616b854cb56cd0319
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-06-08 21:10:34 -0300

    falta lo de categoria y ubicacion x las etiquetas

--- src/main/java/models/entities/fuentes/StrategyAPIREST.java
+++ src/main/java/models/entities/fuentes/StrategyAPIREST.java
@@ -1,6 +1,14 @@
 package models.entities.fuentes;
 
+import models.entities.hecho.Hecho;
+
+import java.util.Map;
+
 public class StrategyAPIREST implements StrategyTipoConexion {
+
     @Override
-    public void agregarHecho() {}
+    public Map<String, Hecho> agregarHecho(String FuenteBase) {
+
+        return null;
+    }
 }
