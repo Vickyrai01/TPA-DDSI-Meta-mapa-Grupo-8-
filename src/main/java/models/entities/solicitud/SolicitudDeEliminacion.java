53ffd347f3eaf165c5d1b055baf8a169013b1f62
Agustin <ascali@frba.utn.edu.ar>
2025-06-10 02:21:48 -0300

    agrego metodos de solicitud de eliminacion

--- src/main/java/models/entities/solicitud/SolicitudDeEliminacion.java
+++ src/main/java/models/entities/solicitud/SolicitudDeEliminacion.java
@@ -3,10 +3,11 @@ package models.entities.solicitud;
 import lombok.Getter;
 import lombok.Setter;
 import models.entities.hecho.Hecho;
-
+import models.entities.hecho.Estado;
+import models.entities.solicitud.DetectorDeSpam;
 import java.time.LocalDateTime;
 
-public class SolicitudDeEliminacion {
+public class SolicitudDeEliminacion implements DetectorDeSpam {
     @Setter
     @Getter
     private Hecho hecho;
@@ -19,7 +20,29 @@ public class SolicitudDeEliminacion {
     @Setter
     @Getter
     private LocalDateTime fechaDeRevision;
-    @Setter
-    @Getter
-    private Boolean esSpam;
+
+    public void aceptarSolicitud(Hecho hecho) {
+        this.aceptada = true;
+        hecho.estado = Estado.INACTIVO;
+    }
+
+    public void rechazarSolicitud() {
+        this.aceptada = false;
+    }
+
+    public void rechazarPorSpam() {
+        if (esSpam(descripcion)) {
+            this.rechazarSolicitud();
+        }
+    }
+
+    public String toString() {
+        return "SolicitudDeEliminacion{" +
+                "hecho=" + hecho +
+                ", descripcion='" + descripcion + '\'' +
+                ", aceptada=" + aceptada +
+                ", fechaDeRevision=" + fechaDeRevision +
+                '}';
+    }
+
 }
