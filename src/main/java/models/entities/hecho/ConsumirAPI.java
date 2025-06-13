4f51e8c9b21344ad60871ec8a760666f5d8d5a3d
Nacho <jmillan@frba.utn.edu.ar>
2025-06-12 23:44:09 -0300

    testeamos consumir API

--- src/main/java/models/entities/hecho/ConsumirAPI.java
+++ src/main/java/models/entities/hecho/ConsumirAPI.java
@@ -2,6 +2,7 @@ package models.entities.hecho;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.cxf.jaxrs.client.WebClient;
 
 import javax.ws.rs.core.Response;
@@ -11,6 +12,7 @@ public class ConsumirAPI {
         WebClient clientUsers = WebClient.create("https://684b1942165d05c5d35b843b.mockapi.io/metamapa/hechos");
 
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         Response response = clientUsers
@@ -26,6 +28,34 @@ public class ConsumirAPI {
 
             for (int i = 0; i < hechos.length; i++) {
                 System.out.println("ID: " + hechos[i].getId());
+            }
+
+        } else {
+            System.out.println("Error response = " + responseBody);
+            throw new Exception("Error en la llamada a /api/user");
+        }
+    }
+
+    public void loguearTitulos() throws Exception {
+        WebClient clientUsers = WebClient.create("https://684b1942165d05c5d35b843b.mockapi.io/metamapa/hechos");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        Response response = clientUsers
+                .header("Content-Type", "application/json")
+                .get();
+
+        int status = response.getStatus();
+        System.out.println("Status: " + status);
+        String responseBody = response.readEntity(String.class);
+        if (status == 200) {
+            System.out.println("response = " + responseBody);
+            HechoResponse[] hechos = objectMapper.readValue(responseBody, HechoResponse[].class);
+
+            for (int i = 0; i < hechos.length; i++) {
+                System.out.println("Título: " + hechos[i].getTitulo());
             }
 
         } else {
--- src/test/java/models/entities/fuentes/ConsumirAPITest.java
+++ src/test/java/models/entities/fuentes/ConsumirAPITest.java
@@ -4,14 +4,12 @@ import models.entities.hecho.ConsumirAPI;
 
 public class ConsumirAPITest {
     public static void main(String[] args) throws Exception {
-//        DistanciaApiCalls distanciaRestClient = new DistanciaApiCalls();
-
-//        String token = distanciaRestClient.obtenerToken("myemail5@mail.com");
 
         ConsumirAPI consumirAPI = new ConsumirAPI();
 
         consumirAPI.loguearUsuariosId();
 
-//        System.out.println(token);
+        consumirAPI.loguearTitulos();
+
     }
 }
