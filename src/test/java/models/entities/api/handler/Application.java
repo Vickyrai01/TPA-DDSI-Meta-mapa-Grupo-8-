c66d1f425f345b2f3c5a2d50c2087eebfe9c2b9e
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-07-05 23:36:12 -0300

    Seeders

--- src/test/java/models/entities/api/handler/Application.java
+++ src/test/java/models/entities/api/handler/Application.java
@@ -2,12 +2,27 @@ package models.entities.api.handler;
 
 import api.handler.*;
 import io.javalin.Javalin;
+import models.entities.hecho.Contribuyente;
+import models.entities.hecho.Coordenadas;
+import models.entities.hecho.Estado;
 import models.entities.hecho.Hecho;
 import models.repository.HechosRepository;
+import models.repository.seeders.HechosRepositorySeeder;
+import models.repository.seeders.SolicitudEliminacioRepositorySeeder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Application {
 
     public static void main(String[] args) {
+
+        HechosRepositorySeeder hechosRepositorySeeder = HechosRepositorySeeder.getInstance();
+        hechosRepositorySeeder.cargarHechosSeeder();
+
+        SolicitudEliminacioRepositorySeeder solicitudEliminacioRepositorySeeder = SolicitudEliminacioRepositorySeeder.getInstance();
+        solicitudEliminacioRepositorySeeder.cargarSolicitudDeEliminacionSeeder();
+
 
         Javalin app = Javalin.create()
                 .get("/", ctx -> ctx.result("Hello World"))
