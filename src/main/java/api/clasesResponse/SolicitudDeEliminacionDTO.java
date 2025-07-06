a0d6492311ea7f60c1f5e2fde1f5eed70d849fbb
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-07-06 00:19:52 -0300

    Ahora le paso el id del hecho

---
+++ src/main/java/api/clasesResponse/SolicitudDeEliminacionDTO.java
@@ -0,0 +1,14 @@
+package api.clasesResponse;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class SolicitudDeEliminacionDTO {
+        public int hecho;
+        public String descripcion;
+        public Boolean aceptada;
+        public LocalDateTime fechaDeRevision;
+
+        public SolicitudDeEliminacionDTO() {}
+    }
+
