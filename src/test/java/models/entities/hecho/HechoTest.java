28bb6e5e7b5330213d8f004c84ad2411f10402b6
Nacho <jmillan@frba.utn.edu.ar>
2025-07-08 12:01:42 -0300

    filtrados testeados

--- src/test/java/models/entities/hecho/HechoTest.java
+++ src/test/java/models/entities/hecho/HechoTest.java
@@ -1,7 +1,6 @@
 package models.entities.hecho;
 
 
-import models.entities.colecciones.criterios.Filtrador;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
