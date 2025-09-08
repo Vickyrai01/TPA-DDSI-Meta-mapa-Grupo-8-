e0a9da4d5d325e4f7efd761cc3002405f17dfd77
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-09-07 22:17:29 -0300

    singleton

--- src/main/java/models/agregador/DetectorDeSpam.java
+++ src/main/java/models/agregador/DetectorDeSpam.java
@@ -2,6 +2,24 @@ package models.agregador;
 
 public class DetectorDeSpam {
 
-    static boolean esSpam(String solicitud)
-    {return false;}
+    static boolean esSpam(String solicitud) {return false;}
+
+    private static volatile DetectorDeSpam instance;
+
+    public DetectorDeSpam() {
+        if (instance != null) {
+            throw new RuntimeException("Usa getInstance() para obtener el Singleton");
+        }
+    }
+
+    public static DetectorDeSpam getInstance() {
+        if (instance == null) {
+            synchronized (DetectorDeSpam.class) {
+                if (instance == null) {
+                    instance = new DetectorDeSpam();
+                }
+            }
+        }
+        return instance;
+    }
 }
