1175196c3ae3a15dfd99bfa4a92c01ca1dafd8df
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-10-06 10:21:23 -0300

    Comienzo del generador de CSV y el module bien hecho

---
+++ servicioDeEstadistica/src/main/java/csv/DEMOGenerarCSV.java
@@ -0,0 +1,12 @@
+package csv;
+
+import java.io.IOException;
+
+public class DEMOGenerarCSV {
+
+    public static void main(String[] args) throws IOException {
+        GeneradorDeEstadisticas generadorDeEstadisticas = new GeneradorDeEstadisticas();
+
+        generadorDeEstadisticas.generarEstadisticas();
+    }
+}
---
+++ servicioDeEstadistica/src/main/java/csv/GeneradorDeEstadisticas.java
@@ -0,0 +1,33 @@
+package csv;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+
+public class GeneradorDeEstadisticas {
+    CSVGenerator csvGenerator = new CSVGenerator();
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+    String timestamp = LocalDateTime.now().format(formatter);
+
+    String path = "servicioDeEstadistica/EstadisticasGeneradas/estadisticas" + timestamp + ".csv";
+
+    public void generarEstadisticas(){
+        String[] headers = { "id", "provincia", "cantidadHechos" };
+
+        List<String[]> rows = List.of(
+                new String[]{ "1", "CABA", "30798" },
+                new String[]{ "2", "Córdoba", "23324" },
+                new String[]{ "3", "San Juan", "28123" }
+        );
+
+        try {
+            csvGenerator.writeCsv(path, rows, headers);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    }
+
