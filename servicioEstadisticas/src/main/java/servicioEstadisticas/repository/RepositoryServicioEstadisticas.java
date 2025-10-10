355b04a8d81137a72cff0f36dc57fea9b0a2160d
Vicky Rai <vraimondo@frba.utn.edu.ar>
2025-10-10 16:19:49 -0300

    Post de hecho funciona correctamente

--- servicioEstadisticas/src/main/java/servicioEstadisticas/repository/RepositoryServicioEstadisticas.java
+++ servicioEstadisticas/src/main/java/servicioEstadisticas/repository/RepositoryServicioEstadisticas.java
@@ -1,5 +1,43 @@
 package servicioEstadisticas.repository;
 
+import servicioEstadisticas.model.Hecho;
+import utils.DBUtils;
+
+import javax.persistence.EntityManager;
+
 public class RepositoryServicioEstadisticas {
+
+    public static String provinciaConMasHechos(){
+
+        return "ups";
+    }
+
+    public static String categoriaMasReportada(){
+
+        return "V";
+    }
+
+
+    public static Integer cantidadSolicitudesEliminacion(){
+        return 1;
+    }
+
+
+    public static void addHecho(Hecho hecho) {
+        EntityManager em = DBUtils.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(hecho);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
 
 }
