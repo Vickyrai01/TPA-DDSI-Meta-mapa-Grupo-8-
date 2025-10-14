84ba53322ec824d46303d1f9e71ba686a851824d
Nacho <jmillan@frba.utn.edu.ar>
2025-10-13 22:59:59 -0300

    agregamos la hora como un atributo adicional

--- core/src/test/java/core/models/entities/repository/TestHechosRepository.java
+++ core/src/test/java/core/models/entities/repository/TestHechosRepository.java
@@ -5,34 +5,54 @@ import core.models.entities.hecho.Categoria;
 import core.models.entities.hecho.Coordenadas;
 import core.models.entities.hecho.Estado;
 import core.models.entities.hecho.Hecho;
+import core.models.repository.CategoriaRepository;
+import core.models.repository.CoordenadasRepository;
 import core.models.repository.HechosRepository;
 import core.models.repository.seeders.HechosRepositorySeeder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TestHechosRepository {
 
+    HechosRepository hechosRepository = HechosRepository.getInstance();
+    CategoriaRepository categoriaRepository = CategoriaRepository.getInstance();
+    CoordenadasRepository coordenadasRepository = CoordenadasRepository.getInstance();
+    HechosRepositorySeeder hechosRepositorySeeder = HechosRepositorySeeder.getInstance();
+
     Coordenadas coordenadas1 = new Coordenadas(123.0, 456.0);
 
     Categoria categoriaIncendio = new Categoria("Incendio");
 
 
-    Hecho hecho1 = new Hecho(coordenadas1, categoriaIncendio,
-            null, null, null, Estado.ACEPTADO, null,
-            LocalDate.now().minusDays(2), LocalDate.now().minusDays(3),
-            TipoFuente.ESTATICA, null, "No hubo heridos, fue por una sartén", "incendio en Casa", null,null);
-    HechosRepository hechosRepository = HechosRepository.getInstance();
-    HechosRepositorySeeder hechosRepositorySeeder = HechosRepositorySeeder.getInstance();
+    Hecho hecho1 = new Hecho(
+            1,
+            coordenadas1,
+            categoriaIncendio,
+            null,
+            null,
+            null,
+            Estado.ACEPTADO,
+            null,
+            LocalDate.now().minusDays(2),
+            LocalDate.now().minusDays(3),
+            TipoFuente.ESTATICA,
+            null,
+            "No hubo heridos, fue por una sartén",
+            "incendio en Casa",
+            null,
+            "34",
+            LocalTime.now());
 
-    Hecho hecho2 = new Hecho( coordenadas1, categoriaIncendio,
+    Hecho hecho2 = new Hecho(2, coordenadas1, categoriaIncendio,
             null, null, null, Estado.ACEPTADO, null,
             LocalDate.now().minusDays(2), LocalDate.now().minusDays(3),
-            TipoFuente.ESTATICA, null, "No hubo heridos, fue por una sartén", "en casa", null,null);
+            TipoFuente.ESTATICA, null, "No hubo heridos, fue por una sartén", "en casa", null, "59", LocalTime.now());
 
 
     @BeforeEach
@@ -42,6 +62,8 @@ public class TestHechosRepository {
 
     @Test
     void agregarARepos(){
+        categoriaRepository.add(categoriaIncendio);
+        coordenadasRepository.add(coordenadas1);
         hechosRepository.add(hecho1);
         hechosRepository.add(hecho2);
     }
