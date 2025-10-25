f2d6085865a794b88fe149c0126e1fab61e296f7
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-10-24 23:58:55 -0300

    seeder hecho mas completo

--- core/src/main/java/core/api/DTO/HechoResumenDTO.java
+++ core/src/main/java/core/api/DTO/HechoResumenDTO.java
@@ -3,13 +3,13 @@ package core.api.DTO;
 import core.models.entities.hecho.Hecho;
 
 public class HechoResumenDTO {
-    public Integer id;
+    public String hash;
     public String nombre;
     public String descripcion;
     public String contribuyente;
 
-    public HechoResumenDTO(Integer id, String titulo, String descripcion, String nombreContribuyente) {
-    this.id = id;
+    public HechoResumenDTO(String hash, String titulo, String descripcion, String nombreContribuyente) {
+    this.hash = hash;
     this.nombre = titulo;
     this.descripcion = descripcion;
     this.contribuyente = nombreContribuyente;
@@ -17,45 +17,23 @@ public class HechoResumenDTO {
 
     public static HechoResumenDTO from(Hecho h) {
         String nombreContribuyente = (h.getContribuyente() != null)
-                ? h.getContribuyente().getNombre()
+                ? h.getContribuyente().getNombreCompleto()
                 : null;
         return new HechoResumenDTO(
-                h.getId(),
+                h.getHash(),
                 h.getTitulo(),
                 h.getDescripcion(),
                 nombreContribuyente
         );
     }
 
-    public Integer getId() {
-        return id;
-    }
+    public String getHash() { return hash; }
+    public String getNombre() { return nombre; }
+    public String getDescripcion() { return descripcion; }
+    public String getContribuyente() { return contribuyente; }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public String getContribuyente() {
-        return contribuyente;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public void setContribuyente(String contribuyente) {
-        this.contribuyente = contribuyente;
-    }
+    public void setHash(String hash) { this.hash = hash; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public void setContribuyente(String contribuyente) { this.contribuyente = contribuyente; }
 }
