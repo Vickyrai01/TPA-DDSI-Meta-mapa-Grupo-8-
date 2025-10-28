95b661a85ca4e16bf1335f011092d247774f30d7
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-10-27 23:43:40 -0300

    generador

--- servicioEstadisticas/src/main/java/csv/DEMOGenerarCSV.java
+++ servicioEstadisticas/src/main/java/csv/DEMOGenerarCSV.java
@@ -6,7 +6,7 @@ public class DEMOGenerarCSV {
 
     public static void main(String[] args) throws IOException {
         GeneradorDeEstadisticas generadorDeEstadisticas = new GeneradorDeEstadisticas();
+        generadorDeEstadisticas.generarTodasEstadisticas();
 
-        generadorDeEstadisticas.generarEstadisticas();
     }
 }
