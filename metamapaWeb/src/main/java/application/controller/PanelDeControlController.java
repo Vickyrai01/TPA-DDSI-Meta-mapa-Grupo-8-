21b406e8998f62c7b3ba80b3703a7e6ffc31e2cb
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-10-29 20:39:20 -0300

    se muestran colecciones bien, faltan algunos atributos

--- metamapaWeb/src/main/java/application/controller/PanelDeControlController.java
+++ metamapaWeb/src/main/java/application/controller/PanelDeControlController.java
@@ -1,16 +1,20 @@
 package application.controller;
 
+import application.service.ColeccionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PanelDeControlController {
+    private final ColeccionService coleccionService;
+    public PanelDeControlController(ColeccionService coleccionService) {
+        this.coleccionService = coleccionService;
+    }
 
     @GetMapping("/admin")
     public String home(Model model) {
-
-        // Si más adelante querés pasar datos a la vista, usá el 'model'
+        model.addAttribute("listaDeColecciones", coleccionService.getAll());
         return "panelDeControl/panelDeControl";
     }
 
