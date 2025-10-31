e5911d458ef488aa953960a329e186f494d3eb66
dani-ojeda003 <cojedacabrera@frba.utn.edu.ar>
2025-10-30 22:33:22 -0300

    lito

--- metamapaWeb/src/main/java/application/controller/HomeController.java
+++ metamapaWeb/src/main/java/application/controller/HomeController.java
@@ -1,26 +1,13 @@
 package application.controller;
 
-import application.service.AdminService;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class HomeController {
-
-    private final AdminService adminService;
-
-    public HomeController(AdminService adminService) {
-        this.adminService = adminService;
-    }
-
     @GetMapping("/")
-    public String home(Model model, Authentication authentication) {
-        boolean esAdmin = adminService.isAdmin(authentication);
-        model.addAttribute("esAdmin", esAdmin);
+    public String home(Model model) {
         return "home/home";
     }
 }
