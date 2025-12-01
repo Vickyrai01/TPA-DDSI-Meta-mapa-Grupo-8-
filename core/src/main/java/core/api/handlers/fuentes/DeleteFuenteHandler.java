b28d58127c3534adbdcde76a8fdf6fe281da45c6
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-12-01 15:59:27 -0300

    Elimino los hechos de esa fuente

--- core/src/main/java/core/api/handlers/fuentes/DeleteFuenteHandler.java
+++ core/src/main/java/core/api/handlers/fuentes/DeleteFuenteHandler.java
@@ -4,6 +4,7 @@ import core.models.entities.colecciones.Coleccion;
 import core.models.entities.fuentes.Fuente;
 import core.models.repository.ColeccionesRepository;
 import core.models.repository.FuentesRepository;
+import core.models.repository.HechosRepository;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
@@ -14,21 +15,18 @@ import java.util.Optional;
 public class DeleteFuenteHandler implements Handler {
 
     private final FuentesRepository fuentesRepository = FuentesRepository.getInstance();
+    HechosRepository hechosRepository = HechosRepository.getInstance();
 
     @Override
     public void handle(@NotNull Context context) throws Exception {
         Integer id = context.pathParamAsClass("id", Integer.class).get();
 
+
+        hechosRepository.eliminarHechosPorIdFuente(id);
         Optional<Fuente> fuenteOptional = fuentesRepository.obtenerTodas().stream()
                 .filter(c -> c.getId() == id)
                 .findFirst();
 
-        if (fuenteOptional.isPresent()) {
-            fuentesRepository.delete(fuenteOptional.get());
-            context.status(200).result("Fuente con ID " + id + " eliminada");
-        } else {
-            context.status(404).result("Fuente con ID " + id + " no encontrada");
-        }
     }
 
 }
