df49ddd70f5177601de62236df4c0e6b7a106a41
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-12-01 12:54:11 -0300

    HTML un poco más arreglado

--- core/src/main/java/core/models/repository/seeders/FuentesRepositorySeeder.java
+++ core/src/main/java/core/models/repository/seeders/FuentesRepositorySeeder.java
@@ -30,8 +30,8 @@ public class FuentesRepositorySeeder {
     public void cargarFuentesSeeder()
     {
         Fuente fuente1 = FuenteFactory.crearFuente("CSV Incendio", null,  TipoFuente.ESTATICA, TipoConexion.CSV);
-        Fuente fuente2 = FuenteFactory.crearFuente("API Choque", null,  TipoFuente.PROXY, TipoConexion.APIREST);
-        Fuente fuente3 = FuenteFactory.crearFuente("Contribucion", null,  TipoFuente.DINAMICA, TipoConexion.APIREST);
+        Fuente fuente2 = FuenteFactory.crearFuente("API Choque", "https://choques.com",  TipoFuente.PROXY, TipoConexion.APIREST);
+        Fuente fuente3 = FuenteFactory.crearFuente("Contribucion",null ,  TipoFuente.DINAMICA, TipoConexion.APIREST);
         fuentesRepository.add(fuente1);
         fuentesRepository.add(fuente2);
         fuentesRepository.add(fuente3);
