3a31fe4ad24b4004af8a0b8215c93d2a9c3a2d57
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-12-01 14:23:40 -0300

    Modal mas lindo para el post

--- metamapaWeb/src/main/java/application/controller/PanelDeControlFuente.java
+++ metamapaWeb/src/main/java/application/controller/PanelDeControlFuente.java
@@ -2,9 +2,14 @@ package application.controller;
 
 import application.service.ColeccionService;
 import application.service.FuenteService;
+import core.api.DTO.FuenteDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PanelDeControlFuente {
@@ -19,4 +24,5 @@ public class PanelDeControlFuente {
         model.addAttribute("listaDeFuentes", fuenteService.getAll());
         return "panelDeControl/panelDeControlFUENTES";
     }
+
 }
