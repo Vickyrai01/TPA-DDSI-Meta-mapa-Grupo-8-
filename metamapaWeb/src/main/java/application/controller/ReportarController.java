4c63169143c02ce3b360547d388ca6413025ba5e
guadi <ghrybalvarez@frba.utn.edu.ar>
2025-12-09 01:04:36 -0300

    logica lista

--- metamapaWeb/src/main/java/application/controller/ReportarController.java
+++ metamapaWeb/src/main/java/application/controller/ReportarController.java
@@ -68,6 +68,7 @@ public class ReportarController {
             @RequestParam("longitud") Double longitud,
             @RequestParam("multimedia") String multimedia,
             @RequestParam(value = "etiquetas", required = false) String etiquetas,
+            @RequestParam(value = "urgente", defaultValue = "false") boolean urgente,
             org.springframework.security.core.Authentication authentication,
             RedirectAttributes ra
     ) {
@@ -117,7 +118,7 @@ public class ReportarController {
             jsonMap.put("etiquetas", etiquetasList);
 
             String json = mapper.writeValueAsString(jsonMap);
-            ResponseEntity<Void> response = reportarService.postearHecho(json);
+            ResponseEntity<Void> response = reportarService.postearHecho(json, urgente);
             if (response.getStatusCode().is2xxSuccessful()) {
                 return "redirect:/reportar?estado=ok";
             } else {
