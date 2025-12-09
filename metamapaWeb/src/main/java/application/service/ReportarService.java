5fae947e5eec987086942520cdd00a36d66bb154
guadi <ghrybalvarez@frba.utn.edu.ar>
2025-12-09 14:01:53 -0300

    boton ejecutar servicio agregacion movido

--- metamapaWeb/src/main/java/application/service/ReportarService.java
+++ metamapaWeb/src/main/java/application/service/ReportarService.java
@@ -40,11 +40,4 @@ public class ReportarService {
                 .block();
     }
 
-    public ResponseEntity<Void> ejecutarAgregacion(){
-        return metamapaApiADMIN.post()
-                .uri("/ejecutarServicio")
-                .retrieve()
-                .toBodilessEntity()
-                .block();
-    }
 }
