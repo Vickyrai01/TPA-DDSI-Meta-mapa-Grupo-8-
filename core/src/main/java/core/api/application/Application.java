f0ec59f5f5452d042ae3ff3e807fa70ec49e6511
Nacho <jmillan@frba.utn.edu.ar>
2025-12-11 16:55:59 -0300

    Interfaz grafica para el GraphQL

--- core/src/main/java/core/api/application/Application.java
+++ core/src/main/java/core/api/application/Application.java
@@ -4,13 +4,17 @@ import core.api.graphql.GraphQLProvider;
 import core.models.repository.ColeccionesRepository;
 import core.models.repository.FuentesRepository;
 import core.models.repository.HechosRepository;
+import core.models.repository.seeders.ColeccionesRepositorySeeder;
 import core.models.repository.seeders.FuentesRepositorySeeder;
+import core.models.repository.seeders.HechosRepositorySeeder;
+import core.models.repository.seeders.SolicitudEliminacioRepositorySeeder;
 import core.observabilidad.RegistroMetricas;
 import io.javalin.Javalin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.util.Map;
 import java.util.UUID;
 
 public class Application {
@@ -18,15 +22,27 @@ public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+        HechosRepositorySeeder hechosRepositorySeeder = HechosRepositorySeeder.getInstance();
+        hechosRepositorySeeder.cargarHechosSeeder();
+
         FuentesRepositorySeeder fuentesRepositorySeeder = FuentesRepositorySeeder.getInstance();
         fuentesRepositorySeeder.cargarFuentesSeeder();
+
+        SolicitudEliminacioRepositorySeeder solicitudEliminacioRepositorySeeder = SolicitudEliminacioRepositorySeeder.getInstance();
+        solicitudEliminacioRepositorySeeder.cargarSolicitudDeEliminacionSeeder();
+
+        ColeccionesRepositorySeeder coleccionesRepositorySeeder = ColeccionesRepositorySeeder.getInstance();
+        coleccionesRepositorySeeder.cargarColeccionesRepositorySeeder();
+
 
         HechosRepository hechosRepo = HechosRepository.getInstance();
         ColeccionesRepository colRepo = ColeccionesRepository.getInstance();
         FuentesRepository fuentesRepo = FuentesRepository.getInstance();
 
         GraphQLProvider graphQLProvider = new GraphQLProvider(hechosRepo, colRepo, fuentesRepo);
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public"); // carpeta en el classpath: src/main/resources/public
+        });
 
         // BEFORE → logs + correlationId + tiempo inicio + métrica de request
         app.before(ctx -> {
@@ -89,11 +105,25 @@ public class Application {
             MDC.clear();
         });
 
+        // Raíz genérica del core
+        app.get("/", ctx -> ctx.result("MetaMapa core API ACTIVA"));
+
         // Endpoint de métricas
         app.get("/metricas", ctx -> ctx.json(RegistroMetricas.snapshot()));
 
-        // Raíz genérica del core
-        app.get("/", ctx -> ctx.result("MetaMapa core API ACTIVA"));
+        app.post("/graphql", ctx -> {
+            System.out.println("BODY RECIBIDO: " + ctx.body());
+            Map<String, Object> body = ctx.bodyAsClass(Map.class);
+
+            String query = (String) body.get("query");
+            Map<String, Object> variables = (Map<String, Object>) body.getOrDefault("variables", Map.of());
+
+            Map<String, Object> result = graphQLProvider.execute(query, variables);
+            ctx.json(result);
+        });
+
+        app.get("/playground", ctx -> ctx.redirect("/graphiql.html"));
+
 
         // Configuramos endpoints públicos y admin sobre la MISMA app
         ApiMetaMapa.configurar(app);       // público
