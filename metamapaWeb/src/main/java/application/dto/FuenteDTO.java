488b0bdcb44e5ce203fe625689cf44dd29893c3e
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2025-12-15 01:34:24 -0300

    refactor paquetes

--- metamapaWeb/src/main/java/application/dto/FuenteDTO.java
+++
@@ -1,7 +0,0 @@
-package application.dto;
-
-public record FuenteDTO( Integer id,
-                         String nombre,
-                         String link,
-                         String tipoFuente,
-                         String strategyTipoConexion) {}
